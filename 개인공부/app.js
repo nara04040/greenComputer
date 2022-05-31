db297a359b6539217beae225522ee017da08ae90
Dev.Jin <nara04040@naver.com>
2022-05-31 17:12:26 +0900

    0531

---
+++ 개인공부/app.js
@@ -0,0 +1,11 @@
+let a = "studing"
+let b = "hard"
+console.log(`${a} is very ${b}`)
+a = "game"
+b = "easy"
+console.log(`${a} is very ${b}`)
+
+const js = "javascript"
+console.log(js)
+js1 = "HTML" 
+console.log(js1)
