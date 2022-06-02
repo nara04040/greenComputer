de4c79eb8334e877b5ad065d0c0b90d16c40248d
Dev.Jin <nara04040@naver.com>
2022-06-02 14:10:59 +0900

    0602am

--- 개인공부/app.js
+++ 개인공부/app.js
@@ -1,11 +1,2 @@
-let a = "studing"
-let b = "hard"
-console.log(`${a} is very ${b}`)
-a = "game"
-b = "easy"
-console.log(`${a} is very ${b}`)
-
-const js = "javascript"
-console.log(js)
-js1 = "HTML" 
-console.log(js1)
+let name = 'nara'
+console.dir(document)
