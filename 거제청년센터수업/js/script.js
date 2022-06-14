22b6326f14b1ed9abfd123d705ed4e52a9b9a279
Dev.Jin <nara04040@naver.com>
2022-06-14 13:20:40 +0900

    점심시간

--- 거제청년센터수업/js/script.js
+++ 거제청년센터수업/js/script.js
@@ -1,12 +1,39 @@
 $(document).ready(function() {
-  new Swiper(".sw-visual", {
-    autoplay: {
-      
+  let sw_visual_start = $('.sw-visual-start');
+  let sw_visual = new Swiper(".sw-visual", {
+    autoplay: {    
+      delay: "2500",
+      disableOnInteraction: false,
     },
+    loop:true,
     effect: "fade",
     pagination: {
-      el: ".swiper-pagination",
+      el: ".sw-visual-control .sw-visual-pagination",
       clickable: true,
     },
   });
+
+  sw_visual_start.click(function(){
+    $(this).toggleClass('sw-visual-stop');
+    let temp = $(this).hasClass('sw-visual-stop');
+    if(temp == true){
+      sw_visual.autoplay.stop();
+      $('.sw-visual-start .material-icons').html('navigate_next')
+    }else {
+      sw_visual.autoplay.start();
+      $('.sw-visual-start .material-icons').html('pause')
+    }
+  })
+
+  // banner slide
+  
+
+  // gotop
+  let go_top = $('.gotop');
+  go_top.click(function(){
+    $('html, body').animate({
+      scrollTop:0,
+    })
+  })
+
 })
