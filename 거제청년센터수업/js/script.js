4268037135284edfe96c17b6677a8d555ce2051d
Dev.Jin <nara04040@naver.com>
2022-06-14 11:28:42 +0900

    3교시

---
+++ 거제청년센터수업/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  new Swiper(".sw-visual", {
+    autoplay: {
+      
+    },
+    effect: "fade",
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+})
