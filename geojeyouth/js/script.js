5fc0b6b1c3b55af50c81903427511ca5b9dd10b1
Dev.Jin <83911617+nara04040@users.noreply.github.com>
2022-06-17 13:07:34 +0900

    점심

--- geojeyouth/js/script.js
+++ geojeyouth/js/script.js
@@ -1,48 +1,68 @@
 // 모든 리소스가 준비가 되었는가?
 // jquery (html , css , js)
-// $(document).ready();
-// vanila js (멀티미디어 요소 포함)
+$(document).ready(function () {
+  // 모바일메뉴
+  // .mb-bt저장해서 활용
+  $('.mb-bt').click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass('mb-bt-open');
+  });
+
+  // 화면 사이즈 체크 .resize라는게 js가 계속 주시하게 만드니까 성능이 떨어질려나?
+  $(window).resize(function () {
+    // 화면너비를 계산한다.
+    let temp = $(window).width();
+    // 1000px 이상이면 모바일 버튼기능 초기화
+    if (temp > 1000) {
+      $('.mb-bt').removeClass('mb-bt-open')
+
+    }
+
+  })
+});
+
+// vanila js (html , css , js 멀티미디어 요소(그림) 포함 준비되면)
 window.onload = function () {
-  let sw_visual= new Swiper(".sw-visual" ,{
+  let sw_visual = new Swiper(".sw-visual", {
     spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-        },
-        pagination: {
-          el: ".sw-visual-pg",
-          clickable:true,
-        },
-  }) 
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".sw-visual-pg",
+      clickable: true,
+    },
+  })
   // 일시멈춤 버튼
   // 현재상태저장
   let slide_now = 'ing';
-  $('.sw-visual-bt').click(function(){
-    if (slide_now == 'ing'){
+  $('.sw-visual-bt').click(function () {
+    if (slide_now == 'ing') {
       // 현재슬라이드 진행중이라면 슬라이드를 멈춰라
       slide_now = 'stop';
       sw_visual.autoplay.stop();
       $(this).find('span').text('play_arrow')
-    }else {
+    } else {
       slide_now = 'ing'
       sw_visual.autoplay.start();
       $(this).find('span').text('pause')
     }
   })
 
-  $('.sw-visual-bt').click(function(){
+  $('.sw-visual-bt').click(function () {
     $(this).toggleClass('sw-visual-bt-play');
     // 슬라이드 토글 시키기
     // 현재 슬라이드 상태
     let temp = $(this).hasClass('sw-visual-bt-play');
-    if(temp){
+    if (temp) {
       // 슬라이더 멈춤
       sw_visual.autoplay.stop()
-    }else{
+    } else {
       // 슬라이더 재생
       sw_visual.autoplay.start()
     }
-    
+
   })
 }
