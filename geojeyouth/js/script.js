6ce1fc00ebc36b02604695d3b4d89d8949237a8f
Dev.Jin <83911617+nara04040@users.noreply.github.com>
2022-06-20 17:33:14 +0900

    6교시

--- geojeyouth/js/script.js
+++ geojeyouth/js/script.js
@@ -6,6 +6,8 @@ $(document).ready(function () {
   $('.mb-bt').click(function (e) {
     e.preventDefault();
     $(this).toggleClass('mb-bt-open');
+    $('.mb-dim').toggleClass('mb-dim-open')
+    $('.mb-wrap').toggleClass('mb-wrap-open')
   });
 
   // 화면 사이즈 체크 .resize라는게 js가 계속 주시하게 만드니까 성능이 떨어질려나?
@@ -15,7 +17,8 @@ $(document).ready(function () {
     // 1000px 이상이면 모바일 버튼기능 초기화
     if (temp > 1000) {
       $('.mb-bt').removeClass('mb-bt-open')
-
+      $('.mb-dim').removeClass('mb-dim-open')
+      $('.mb-wrap').removeClass('mb-wrap-open')
     }
 
   })
@@ -65,4 +68,52 @@ window.onload = function () {
     }
 
   })
+  // 배너슬라이드
+  let sw_banner = new Swiper('.sw-banner', {
+      loop: true,
+      slidesPerView: 3,
+      spaceBetween: 20,
+      navigation: {
+        prevEl: '.sw-banner-prev',
+        nextEl: '.sw-banner-next'
+      },
+      autoplay: {
+        delay: 1000,
+        disableOnInteraction: false
+      },
+      breakpoints : {
+        1400: {
+          slidesPerView: 6
+        },
+        1260: {
+          slidesPerView: 5
+        },
+        1000: {
+          slidesPerView: 4
+        },
+        860: {
+          slidesPerView: 3
+        }
+      }
+  })
+// 슬라이드 멈춤 버튼
+$('.sw-banner-pause').click(function () {
+  // 현재 span 태그 안쪽 글자를 읽는다.
+  let temp = $(this).find('span').text();
+  if (temp == 'pause') {
+    $(this).find('span').text('play_arrow');
+    sw_banner.autoplay.stop()
+  } else {
+    $(this).find('span').text('pause');
+    sw_banner.autoplay.start()
+  }
+});
+  //화면 위로 이동
+  $('.gotop').click(function() {
+
+    $('html').animate({
+      scrollTop: 0
+    });
+  });
+
 }
