5452ef98028b06107cc3c68837e0bac45d63367e
Dev.Jin <83911617+nara04040@users.noreply.github.com>
2022-06-21 17:43:17 +0900

    마시그래이반응형,input

--- masigray/js/script.js
+++ masigray/js/script.js
@@ -13,5 +13,20 @@ window.onload = function () {
             nextEl: '.sw-visual-next',
         }
     });
-
+    // items 슬라이드
+    new Swiper('.sw-items', {
+        loop: true,
+        spped: 800,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            prevEl: '.sw-items-prev',
+            nextEl: '.sw-items-next',
+        },
+        pagination: {
+            el: '.sw-items-pg'
+        }
+    });
 };
